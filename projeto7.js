698cd89a5de216e2fec271274f281160ad26146f
Pedro001979 <144935572+Pedro001979@users.noreply.github.com>
2024-01-22 14:53:19 -0300

    Add files via upload

---
+++ projeto7.js
@@ -0,0 +1,25 @@
+/*Um Projeto Simples utilizando Orientação ao Objeto em JS, Para Gravar os detalhes de um Carro e Dizer quanto ele gastaria em uma Viagem.
+*/
+
+
+
+class Carros {
+    marca;
+    cor;
+    gastoMEdioPorKm;
+
+    constructor(marca, cor, gastoMEdioPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.gastoMEdioPorKm = gastoMEdioPorKm;
+    }
+
+    calcularGatoDoPercurso(distanciaEmKm, precoDoCombustivel) {
+        return distanciaEmKm * this.gastoMEdioPorKm * precoDoCombustivel
+    }
+};
+
+const Kwid = new Carros('Renaut', 'Preta', 1 / 15, 7);
+const Palio = new Carros('Fiat', 'Azul', 1 / 10);
+console.log(Kwid.calcularGatoDoPercurso(100, 5));
+console.log(Palio.calcularGatoDoPercurso(100, 5));
