f3c4cff7e2e08b1b283c1b615987afc6b526d8cf
Pedro001979 <144935572+Pedro001979@users.noreply.github.com>
2024-01-22 14:53:34 -0300

    Update projeto7.js

--- projeto7.js
+++ projeto7.js
@@ -1,8 +1,3 @@
-/*Um Projeto Simples utilizando Orientação ao Objeto em JS, Para Gravar os detalhes de um Carro e Dizer quanto ele gastaria em uma Viagem.
-*/
-
-
-
 class Carros {
     marca;
     cor;
